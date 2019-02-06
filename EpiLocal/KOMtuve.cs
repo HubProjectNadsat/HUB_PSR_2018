f5a17e2d16a7e93b019df06caa63ae58a5b7188b
Min-Soo <min-soo.ko@epitech.eu>
2019-02-06 17:24:55 +0100

    Basic Discord Bot
    디스코드 보트

---
+++ EpiLocal/KOMtuve.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace EpiLocal
+{
+    class KOMtuve : ModuleBase
+    {
+        [Command("local")]
+        async Task local(params string[] args)
+        {
+            await ReplyAsync("le local est ouvert");
+        }
+    }
+}
